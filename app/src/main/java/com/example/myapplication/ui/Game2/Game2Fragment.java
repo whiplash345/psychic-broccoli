2a45b8c04475e3f8d1214c2a837d657c3fc0580f
Noah Bisping <NEBisping@student.fullsail.edu>
2024-08-26 11:33:47 -0400

    Merge branch 'Noah-Main-Menu'

---
+++ app/src/main/java/com/example/myapplication/ui/Game2/Game2Fragment.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.ui.Game2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.example.myapplication.R;
+
+public class Game2Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game2, container, false);
+    }
+}
